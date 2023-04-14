638007f48955baea95c8e561c0cacda46c5c6373
Tim Stahlhut <stahta01@gmail.com>
2023-04-14 15:08:23 -0400

    Use wxSmith to regenerate CommitAllDialog.cpp/wxs files

--- src/CommitAllDialog.cpp
+++ src/CommitAllDialog.cpp
@@ -2,8 +2,8 @@
 
 //(*InternalHeaders(CommitAllDialog)
 #include <wx/button.h>
-#include <wx/string.h>
 #include <wx/intl.h>
+#include <wx/string.h>
 //*)
 
 //(*IdInit(CommitAllDialog)
@@ -19,8 +19,8 @@ END_EVENT_TABLE()
 CommitAllDialog::CommitAllDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size)
 {
 	//(*Initialize(CommitAllDialog)
-	wxBoxSizer* BoxSizer2;
 	wxBoxSizer* BoxSizer1;
+	wxBoxSizer* BoxSizer2;
 	wxStdDialogButtonSizer* StdDialogButtonSizer1;
 
 	Create(parent, id, _("Commit all changes"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
@@ -31,15 +31,14 @@ CommitAllDialog::CommitAllDialog(wxWindow* parent,wxWindowID id,const wxPoint& p
 	StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Comment: "), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
 	BoxSizer2->Add(StaticText1, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	Comment = new wxTextCtrl(this, ID_COMMENT, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_COMMENT"));
-	BoxSizer2->Add(Comment, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-	BoxSizer1->Add(BoxSizer2, 1, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer2->Add(Comment, 1, wxALL|wxEXPAND, 5);
+	BoxSizer1->Add(BoxSizer2, 1, wxEXPAND, 5);
 	StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
 	StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_OK, wxEmptyString));
 	StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_CANCEL, wxEmptyString));
 	StdDialogButtonSizer1->Realize();
-	BoxSizer1->Add(StdDialogButtonSizer1, 1, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer1->Add(StdDialogButtonSizer1, 1, wxEXPAND, 5);
 	SetSizer(BoxSizer1);
-	BoxSizer1->Fit(this);
 	BoxSizer1->SetSizeHints(this);
 	//*)
 	
